a92d3c2dd3c19341184b55c854ade34c4be07a64
pingfengafei <intern@maxent-inc.com>
2016-10-31 12:49:52 +0800

    重构了App和Index页面

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import {Router, Route, IndexRoute, Link, browserHistory} from 'react-router';
 import './foundation/test/test';
 
 //pageContainer
+import AppContainer from './container/AppContainer';
 import IndexRoutePageContainer from './container/IndexRoutePage/IndexRoutePageContainer';
 
 
@@ -21,39 +22,11 @@ import {createStore} from 'redux';
 import './index.less';
 
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {num: 1};
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		this.setState({num: ++this.state.num});
-	}
-
-	render() {
-		return (
-			<div className="wrap">
-				<div className="top-nav">我是头部</div>
-				<div className="left-nav">
-					左侧导航栏,因为这里默认用了bootstrap,导致初次看到页面时,误以为fixed和absolute没有脱离正常文本流
-				</div>
-				<div className="wrap-content">
-					{this.props.children}
-				</div>
-			</div>);
-		return <div></div>;
-
-	}
-}
-
-
 //let rootStore = createStore();
 
 render(
 	<Router history={browserHistory}>
-		<Route path="/" component={App}>
+		<Route path="/" component={AppContainer}>
 			<IndexRoute component={IndexRoutePageContainer}/>
 			{/*<Route path="ref" component={TestRef}/>*/}
 			{/*<Route path="input" component={TestInput}/>*/}
